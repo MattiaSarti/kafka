05403836db7894bde7851e0baba9108f6c37843c
MattiaSarti <nmattia1@gmail.com>
2022-12-15 14:05:38 +0100

    ❄️ ADD - temporary link 🎄

--- consumer/application.py
+++ consumer/application.py
@@ -1,5 +1,6 @@
 """
 TODO
+https://developer.confluent.io/quickstart/kafka-docker/
 https://docs.confluent.io/kafka-clients/python/current/overview.html#ak-consumer
 https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#pythonclient-producer
 """
