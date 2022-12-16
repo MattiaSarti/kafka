28406aedd420de7b6f877dda24a0f6bb309b1bb7
MattiaSarti <nmattia1@gmail.com>
2022-12-16 12:25:50 +0100

    ❄️ UPDATE - docs 🎄

--- consumer/application.py
+++ consumer/application.py
@@ -1,5 +1,6 @@
 """
-TODO
+Live stock price chart application, consuming latest price change events.
+
 https://developer.confluent.io/quickstart/kafka-docker/
 https://docs.confluent.io/kafka-clients/python/current/overview.html#ak-consumer
 https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html#pythonclient-producer
